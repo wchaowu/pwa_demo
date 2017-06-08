9f6b039787d3249b7772314fc9ec88316fd70cc6
wuchaowu <wuchaowu@jd.com>
2017-06-08 12:44:34 +0800

    update set serviceWorker

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ window.addEventListener('beforeinstallprompt', function(e) {
     if(navigator.serviceWorker){
         // For security reasons, a service worker can only control the pages that are in the same directory level or below it. That's why we put sw.js at ROOT level.
         navigator.serviceWorker
-            .register('/service_worker_main.js')
+            .register('service_worker_main.js')
             .then((registration) => {console.log('Service Worker Registered. ', registration)})
             .catch((error) => {console.log('ServiceWorker registration failed: ', error)})
     }
