7ba8afdb533d55567fedc39690a8829095785dfd
wuchaowu <wuchaowu@jd.com>
2017-06-08 12:25:06 +0800

    Progressive Web Apps

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
+
+  // e.userChoice will return a Promise. 
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
+
+    if(navigator.serviceWorker){
+        // For security reasons, a service worker can only control the pages that are in the same directory level or below it. That's why we put sw.js at ROOT level.
+        navigator.serviceWorker
+            .register('/service_worker_main.js')
+            .then((registration) => {console.log('Service Worker Registered. ', registration)})
+            .catch((error) => {console.log('ServiceWorker registration failed: ', error)})
+    }
---
+++ service_worker_main.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const CACHE_NAME = 'cache-v1';
+// The files we want to cache
+const resourceList = [
+  '/',
+  'index.html'
+];
+
+self.onfetch = (e) => {
+  e.respondWith(new Response('Hello World!'))
+}
+self.oninstall = (e) => {
+  e.waitUntil(
+    caches.open('installation')
+      .then(cache =>  cache.addAll([
+        './',     
+        './index.js'
+      ]))
+  )
+});
+
+self.onfetch = (e) => {
+  const fetched = fetch(e.request)
+  const cached = caches.match(e.request)
+
+  e.respondWith(
+    fetched.catch(_ => cached)
+  )
+}
+
+self.addEventListener('push', event => {
+  event.waitUntil(
+    // Process the event and display a notification.
+    self.registration.showNotification("Hey!")
+  );
+});
+
+self.addEventListener('notificationclick', event => {  
+  // Do something with the event  
+  event.notification.close();  
+});
+
+self.addEventListener('notificationclose', event => {  
+  // Do something with the event  
+});
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+    return cache.addAll(resourceList);
+  }));
+});
+
+function addToCache(cacheName, resourceList) {
+  caches.open(cacheName).then(cache => {
+    return cache.addAll(resourceList);
+  });
+}
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => {
+    return response || fetch(event.request);
+  }));
+});
